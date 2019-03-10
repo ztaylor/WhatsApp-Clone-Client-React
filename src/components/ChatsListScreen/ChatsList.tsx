b12b9ff13b7fa52d24584b7aeb4392a9c08655a5
DAB0mB <emanor6@gmail.com>
2019-03-10 16:41:59 +0800

    Step 2.5: Add style with styled-components

--- src/components/ChatsListScreen/ChatsList.tsx
+++ src/components/ChatsListScreen/ChatsList.tsx
@@ -1,25 +1,79 @@
 import { List, ListItem } from '@material-ui/core'
 import moment from 'moment'
 import * as React from 'react'
+import styled from 'styled-components'
 import { chats } from '../../db'
 
+const Container = styled.div `
+  height: calc(100% - 56px);
+  overflow-y: overlay;
+`
+
+const StyledList = styled(List) `
+  padding: 0 !important;
+`
+
+const StyledListItem = styled(ListItem) `
+  height: 76px;
+  padding: 0 15px;
+  display: flex;
+`
+
+const ChatPicture = styled.img `
+  height: 50px;
+  width: 50px;
+  object-fit: cover;
+  border-radius: 50%;
+`
+
+const ChatInfo = styled.div `
+  width: calc(100% - 60px);
+  padding: 15px 0;
+  margin-left: 10px;
+  border-bottom: 0.5px solid silver;
+  position: relative;
+`
+
+const ChatName = styled.div `
+  margin-top: 5px;
+`
+
+const MessageContent = styled.div `
+  color: gray;
+  font-size: 15px;
+  margin-top: 5px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`
+
+const MessageDate = styled.div `
+  position: absolute;
+  color: gray;
+  top: 20px;
+  right: 0;
+  font-size: 13px;
+`
+
 const ChatsList = () => (
-  <div>
-    <List>
+  <Container>
+    <StyledList>
       {chats.map((chat) => (
-        <ListItem key={chat.id} button>
-          <img src={chat.picture} />
-          <div>{chat.name}</div>
-          {chat.lastMessage && (
-            <React.Fragment>
-              <div>{chat.lastMessage.content}</div>
-              <div>{moment(chat.lastMessage.createdAt).format('HH:mm')}</div>
-            </React.Fragment>
-          )}
-        </ListItem>
+        <StyledListItem key={chat.id} button>
+          <ChatPicture src={chat.picture} />
+          <ChatInfo>
+            <ChatName>{chat.name}</ChatName>
+            {chat.lastMessage && (
+              <React.Fragment>
+                <MessageContent>{chat.lastMessage.content}</MessageContent>
+                <MessageDate>{moment(chat.lastMessage.createdAt).format('HH:mm')}</MessageDate>
+              </React.Fragment>
+            )}
+          </ChatInfo>
+        </StyledListItem>
       ))}
-    </List>
-  </div>
+    </StyledList>
+  </Container>
 )
 
 export default ChatsList
--- src/components/ChatsListScreen/ChatsNavbar.tsx
+++ src/components/ChatsListScreen/ChatsNavbar.tsx
@@ -1,10 +1,18 @@
 import { Toolbar } from '@material-ui/core'
 import * as React from 'react'
+import styled from 'styled-components'
+
+const Container = styled(Toolbar) `
+  background-color: var(--primary-bg);
+  color: var(--primary-text);
+  font-size: 20px;
+  line-height: 40px;
+`
 
 const ChatsNavbar = () => (
-  <Toolbar>
+  <Container>
     Whatsapp Clone
-  </Toolbar>
+  </Container>
 )
 
 export default ChatsNavbar
