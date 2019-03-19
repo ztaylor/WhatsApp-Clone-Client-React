cb8c02b328aa84310fb46197d5844cbddddcb284
DAB0mB <emanor6@gmail.com>
2019-03-20 03:16:14 +0800

    Step 6.4: Test new navigation logic

--- src/components/ChatsListScreen/ChatsList.test.tsx
+++ src/components/ChatsListScreen/ChatsList.test.tsx
@@ -1,10 +1,14 @@
+import { createBrowserHistory } from 'history'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { cleanup, render, waitForDomChange } from 'react-testing-library'
+import { cleanup, render, fireEvent, wait, waitForDomChange } from 'react-testing-library'
 import ChatsList from './ChatsList'
 
 describe('ChatsList', () => {
-  afterEach(cleanup)
+  afterEach(() => {
+    cleanup()
+    window.location.pathname = '/'
+  })
 
   it('renders fetched chats data', async () => {
     fetch.mockResponseOnce(JSON.stringify({
@@ -33,6 +37,39 @@ describe('ChatsList', () => {
       expect(getByTestId('picture')).toHaveAttribute('src', 'https://localhost:4000/picture.jpg')
       expect(getByTestId('content')).toHaveTextContent('Hello')
       expect(getByTestId('date')).toHaveTextContent('08:00')
+    }
+  })
+
+  it('should navigate to the target chat room on chat item click', async () => {
+    fetch.mockResponseOnce(JSON.stringify({
+      data: {
+        chats: [
+          {
+            id: 1,
+            name: 'Foo Bar',
+            picture: 'https://localhost:4000/picture.jpg',
+            lastMessage: {
+              id: 1,
+              content: 'Hello',
+              createdAt: new Date(0),
+            },
+          },
+        ],
+      },
+    }))
+
+    const history = createBrowserHistory()
+
+    {
+      const { container, getByTestId } = render(<ChatsList history={history} />)
+
+      await waitForDomChange({ container })
+
+      fireEvent.click(getByTestId('chat'))
+
+      await wait(() =>
+        expect(history.location.pathname).toEqual('/chats/1')
+      )
     }
   })
 })
