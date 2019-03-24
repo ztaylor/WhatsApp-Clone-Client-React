a0e9fb9cbc230c0f5f01eeaf40bd907c1916fc29
DAB0mB <emanor6@gmail.com>
2019-03-24 22:21:10 +0800

    Step 8.1: Send message with a GraphQL mutation

--- src/components/ChatRoomScreen/index.tsx
+++ src/components/ChatRoomScreen/index.tsx
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 import * as React from 'react'
 import { useCallback } from 'react'
-import { useApolloClient, useQuery } from 'react-apollo-hooks'
+import { useApolloClient, useQuery, useMutation } from 'react-apollo-hooks'
 import styled from 'styled-components'
 import ChatNavbar from './ChatNavbar'
 import MessageInput from './MessageInput'
@@ -29,28 +29,47 @@ const getChatQuery = gql `
   }
 `
 
+const addMessageMutation = gql `
+  mutation AddMessage($chatId: ID!, $content: String!) {
+    addMessage(chatId: $chatId, content: $content) {
+      id
+      content
+      createdAt
+    }
+  }
+`
+
 const ChatRoomScreen = ({ history, match }) => {
   const { params: { chatId } } = match
   const client = useApolloClient()
   const { data: { chat } } = useQuery(getChatQuery, {
     variables: { chatId }
   })
+  const addMessage = useMutation(addMessageMutation)
 
   const onSendMessage = useCallback((content) => {
-    const message = {
-      id: chat.messages.length + 1,
-      createdAt: Date.now(),
-      content,
-    }
-
-    client.writeQuery({
-      query: getChatQuery,
-      variables: { chatId },
-      data: {
-        chat: {
-          ...chat,
-          messages: chat.messages.concat(message),
-        },
+    addMessage({
+      variables: { chatId, content },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        addMessage: {
+          __typename: 'Message',
+          id: Math.random().toString(36).substr(2, 9),
+          createdAt: new Date(),
+          content,
+        }
+      },
+      update: (client, { data: { addMessage } }) => {
+        client.writeQuery({
+          query: getChatQuery,
+          variables: { chatId },
+          data: {
+            chat: {
+              ...chat,
+              messages: chat.messages.concat(addMessage),
+            },
+          },
+        })
       },
     })
   }, [chat])
