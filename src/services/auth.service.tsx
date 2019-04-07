bb52b78f65cc79a458af502df5c865a9915d46a8
DAB0mB <emanor6@gmail.com>
2019-04-07 18:09:45 +0800

    Step 13.2: Update auth service to call signIn mutation

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -1,7 +1,9 @@
+import { parse as parseCookie } from 'cookie'
 import * as React from 'react'
 import { Redirect } from 'react-router-dom'
 import client from '../client'
 import { useCacheService } from './cache.service'
+import gql from 'graphql-tag';
 
 export const withAuth = (Component: React.ComponentType) => {
   return (props) => {
@@ -23,19 +25,25 @@ export const withAuth = (Component: React.ComponentType) => {
   }
 }
 
-export const signIn = (authToken) => {
-  document.cookie = `authToken=${authToken}`
-
-  // This will become async in the near future
-  return Promise.resolve()
+export const signIn = ({ username, password }) => {
+  return client.mutate({
+    mutation: gql`
+      mutation signIn($username: String!, $password: String!) {
+        signIn(username: $username, password: $password) {
+          id
+        }
+      }
+    `,
+    variables: {
+      username,
+      password
+    }
+  });
 }
 
 export const signOut = () => {
-  // "expires" represents the lifespan of a cookie. Beyond that date the cookie will
-  // be deleted by the browser. "expires" cannot be viewed from "document.cookie"
   document.cookie = `authToken=;expires=${new Date(0)}`
 
-  // Clear cache
   return client.clearStore()
 }
 
