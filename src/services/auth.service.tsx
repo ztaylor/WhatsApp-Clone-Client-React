c8b2efb52c7d36aa411dd727f20dad4a704b2a87
DAB0mB <emanor6@gmail.com>
2019-04-07 18:09:45 +0800

    Step 13.2: Update auth service to call REST endpoint

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -1,3 +1,4 @@
+import { parse as parseCookie } from 'cookie'
 import * as React from 'react'
 import { Redirect } from 'react-router-dom'
 import client from '../client'
@@ -23,19 +24,25 @@ export const withAuth = (Component: React.ComponentType) => {
   }
 }
 
-export const signIn = (authToken) => {
-  document.cookie = `authToken=${authToken}`
-
-  // This will become async in the near future
-  return Promise.resolve()
+export const signIn = ({ username, password }) => {
+  return fetch(`${process.env.REACT_APP_SERVER_URL}/sign-in`, {
+    method: 'POST',
+    body: JSON.stringify({ username, password }),
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+  }).then(res => {
+    if (res.status >= 400) {
+      return res.text().then(e => Promise.reject(e))
+    }
+  })
 }
 
 export const signOut = () => {
-  // "expires" represents the lifespan of a cookie. Beyond that date the cookie will
-  // be deleted by the browser. "expires" cannot be viewed from "document.cookie"
   document.cookie = `authToken=;expires=${new Date(0)}`
 
-  // Clear cache
   return client.clearStore()
 }
 
